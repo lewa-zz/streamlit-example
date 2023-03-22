da0556a18e5309cd509fbde931e07d24bc2957b7
rico <fly2me@vip.qq.com>
2023-03-22 21:50:24 +0800

    中介网上取数据

---
+++ codegenpy1.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+import pandas as pd
+import re
+import sqlite3
+from datetime import datetime 
+
+data = {'序号':[],'href':[],'项目':[],'项目业主':[],'服务类型':[],'中选机构':[],'金额':[],'服务内容':[],'时间':[]}  #data= dict()
+base_url:str = r"https://ygp.gdzwfw.gov.cn"
+
+def write_excel(data):
+    '''
+    写一个全新的文件也很简单：
+    data = {'name': ['zs', 'ls', 'ww'], 'age': [
+        11, 12, 13], 'gender': ['man', 'man', 'woman']}
+    '''
+    df = pd.DataFrame(data)
+    df.to_excel('网上中介.xlsx')
+
+def popup(context,href):
+    new_page = context.new_page()
+    new_page.goto(href)
+    new_page.wait_for_timeout(2000)  
+
+    #pop up a new_page to locate the amo
+    _do = True
+    while _do:
+        with context.expect_page() as new_page2_info:
+            new_page.locator('li:has-text("采购项目名称")').locator("a").click() # Opens a new tab
+        new_page2 = new_page2_info.value
+        new_page2.wait_for_load_state()
+        new_page2.wait_for_timeout(4000) 
+        print(new_page2.url)
+        _do = False
+        if  new_page2.url == r"https://ygp.gdzwfw.gov.cn/zjfwcs/gd-zjcs-pub/home":
+                _do = True
+                new_page2.close()
+
+    amo = new_page2.locator('div > ul > li:has-text("项目规模")').locator("div")
+    #amo = new_page2.get_by_role("li").filter(has_text="项目规模")
+
+    print("项目规模:%s"%amo.text_content())
+    _kk = re.compile(r'\d+\.?\d+') #正则中小数点加\转义,不然.就识别任一字符
+    kk = re.findall(_kk,amo.text_content())
+    amo = float(kk[0]) if len(kk) != 0 else 0
+    #data["金额"].append(amo)
+    content = new_page2.locator('li:has-text("服务内容")').locator("div").text_content()
+    #data["服务内容"].append(content)
+
+    new_page2.close()
+    new_page.close()
+    return locals()
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://ygp.gdzwfw.gov.cn/zjfwcs/gd-zjcs-pub/bidResultNotice")
+    page.wait_for_timeout(3000)
+    #page.locator("//html/body/div[6]/div[2]/div/span").click() #展开选择面板
+    #page.locator("div").filter(has_text="展开").first.click()
+    page.locator("span").filter(has_text="展开").click()
+    #选择东莞市
+    page.locator("#divisionCodePlugs").get_by_placeholder("请选择").click()
+    page.get_by_role("listitem", name="东莞市").click()
+    page.get_by_role("button", name="确定").click()
+
+    #填入查询日期
+    page.get_by_placeholder("选取时间").click()
+    page.get_by_placeholder("选取时间").press("Enter") #要加个回车才能fill入数据
+    page.get_by_placeholder("选取时间").fill("2022-01-01")
+    page.get_by_placeholder("选取时间").press("Tab")
+    # page.locator("#publishDateEnd").click()
+    # page.locator("#publishDateEnd").fill("2022-01-31")
+    page.get_by_placeholder("至").click()
+    page.get_by_placeholder("至").press("Enter") 
+    page.get_by_placeholder("至").fill("2022-01-15")
+    page.get_by_placeholder("至").press("Tab")
+
+    #选择服务项目
+    page.get_by_text("展开").click()
+    page.locator("label").filter(has_text="工程监理").locator("span").nth(1).click()
+    page.locator("label").filter(has_text="信息系统工程监理").locator("span").nth(1).click()
+    page.locator("#serviceType").get_by_text("收起").click()
+
+    page.click("#searchButton")
+    page.wait_for_timeout(4000)
+    rec_total = page.locator("#totalElementsView").text_content()
+    rec_total = re.findall(r'\d+',rec_total) 
+    rec_total = 0 if not rec_total else int(rec_total[0]) #列表空为FALSE,取0,不空,提出数字
+        
+    #根据查询到的结果进行资料收集
+    i = 0
+     #循环取出记录,打开新页面.
+    while i < rec_total:
+        #定位表格的行,然后遍历
+        tr = page.locator('//*[@id="resultPannel"]/table/tbody/tr') #[1]/td[1]'.locator("a")
+        #print(li.text_content())
+        #j+=1
+        for row in tr.all():
+            data['序号'].append(i)
+            #定位每行下面的列TD
+            td = row.locator("td").all()
+            # tdo = expect(row.locator("td")).to_have_count(5)期待有5列
+            alink = td[0].get_by_role("link") #或ahref = row.locator("a")
+            ahref = base_url+str(alink.get_attribute("href"))
+            data["href"].append(ahref)
+            data["项目"].append(alink.text_content())
+            data["项目业主"].append(td[1].inner_text())
+            data["服务类型"].append(td[2].inner_text())
+            data["中选机构"].append(td[3].inner_text())
+            data["时间"].append(td[4].inner_text())
+            #print("url is %s ,content is :%s"%(td[0].get_attribute("href"),td[0].text_content))
+            i += 1
+            di:dict = popup(context,ahref)
+            data["金额"].append(di["amo"])
+            data["服务内容"].append(di["content"])
+        
+        if i < rec_total: 
+            page.get_by_text(">",exact=True).click
+            page.wait_for_timeout(2000)
+
+    write_excel(data)
+    context.close()
+    browser.close()
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        run(playwright)
---
+++ 财政网RPA.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+import time
+import requests
+from PIL import Image
+from io import BytesIO
+import ddddocr
+import pandas as pd
+from pandas import DataFrame
+import re
+import sqlite3
+from datetime import datetime 
+
+def write_excel(data):
+    '''
+    写一个全新的文件也很简单：
+    data = {'name': ['zs', 'ls', 'ww'], 'age': [
+        11, 12, 13], 'gender': ['man', 'man', 'woman']}
+    '''
+    df = pd.DataFrame(data)
+    df.to_excel('new.xlsx')
+
+def create_db():
+    # 创建与数据库的连接
+    conn = sqlite3.connect('db.sqlite3')
+    # 建表的sql语句
+    sql_text_1 = '''CREATE TABLE scores
+            (姓名 TEXT,
+                班级 TEXT,
+                性别 TEXT,
+                语文 NUMBER,
+                数学 NUMBER,
+                英语 NUMBER);'''
+    #创建一个游标 cursor
+    cur = conn.cursor()
+    # 执行sql语句
+    cur.execute(sql_text_1)
+    # 插入单条数据
+    sql_text_2 = "INSERT INTO scores VALUES('A', '一班', '男', 96, 94, 98)"
+    cur.execute(sql_text_2)
+    # 提交改动的方法
+    conn.commit()
+    # 关闭游标
+    cur.close()
+    # 关闭连接
+    conn.close()
+
+def popup(context,row):
+    with context.expect_page() as new_page_info:
+        row.click() # Opens a new tab
+    new_page = new_page_info.value
+    new_page.wait_for_load_state()    
+    #print(new_page.title())
+    prj_name       = new_page.locator('#print-content > p').text_content() #项目名称
+    budget      = new_page.locator("#f_budget").text_content() #预算金额字符串
+    budget      = float(budget[len("预算金额："):-2].replace(",","")) #转成float,取出数值，过滤逗号
+
+    buye_type   = new_page.locator("#f_catalogueNameList").text_content() #采购品目
+    buyer_plan  = new_page.locator("#f_openTenderCode").text_content()  #采购计划编号
+    buyer_time  = new_page.locator("#f_noticeTime").text_content()  #发布时间: xxxx-xx-xx hh:mm:ss
+    buyer_time  = buyer_time[len("发布时间："):]           #取出日期字串
+    buyer_time  = datetime.strptime(buyer_time,'%Y-%m-%d %H:%M:%S') #转换成日期类型
+    
+    #agaent =  new_page.locator('//*[@id="noticeArea"]').locator('//*[@class="innercontent"]')
+    #方法二、通过">" 通过上级> 下级 定位
+    agaent = new_page.locator('#noticeArea > .innercontent > p').all()
+    # for a in agaent:
+    #     print(a.text_content())
+    #agaent.screenshot(path=f"采购人信息%d.png"%i)
+    buyer         = agaent[0].text_content()
+    buyer_address = agaent[1].text_content()
+    buyer_phone   = agaent[2].text_content()
+    #print("采购人：%s 地址：%s 电话：%s "%(buyer,buyerAddress,buyerPhone))
+    new_page.close()
+    return locals()
+
+def run(playwright: Playwright) -> None:
+    data = {'序号':[],'href':[],'项目':[],'项目1':[],'局办':[],'类目':[],'采购编号':[],'金额':[],'时间':[]}  #data= dict()
+    base_url:str = r"https://gdgpo.czt.gd.gov.cn/"
+
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://gdgpo.czt.gd.gov.cn/cms-gd/site/guangdong/xmcggg/index.html")
+    page.wait_for_timeout(1000)
+    #page.wait_for_selector("text=请选择区域")
+    #time.sleep(8) 不用这个方法等
+    page.get_by_placeholder("请选择区域").click()
+    page.get_by_placeholder("请选择区域").fill("东莞市")
+    page.get_by_title("东莞市").click()
+    page.locator("#noticeTypeList").get_by_text("中标（成交）结果公告").dblclick()
+    page.get_by_placeholder("请选择开始时间").click()
+    page.get_by_placeholder("请选择开始时间").fill("2022-01-01 00:00:00")
+    page.get_by_placeholder("请选择结束时间").click()
+    page.get_by_placeholder("请选择结束时间").fill("2022-12-31 23:59:59")
+
+    #page.locator("#code_img").screenshot(path=f"screenshot.png")
+    #载图id为code_img的图片，放入内存中，待识别用
+    code_img = page.locator("#code_img").screenshot()
+    ocr =  ddddocr.DdddOcr(old=True)
+    code_text =  ocr.classification(code_img)
+    #识别出验证码,并填入
+    page.get_by_placeholder("请输入验证码").click()
+    page.get_by_placeholder("请输入验证码").fill(code_text)
+    page.get_by_role("button", name="查询").click()
+    page.wait_for_timeout(2000)
+    #==================================================================
+    #查询出来后，读取表格的内容
+
+    #查找ID=pagination下面的LI标签，合多少条记录
+    rec_total = page.locator('#pagination').locator('li:has-text("合计")').text_content()
+    rec_total = int(re.findall(r'\d+',rec_total)[0])
+    page_amo =page.locator('#pagination').locator('li:has-text("共")').text_content()
+   
+   
+    kk = re.compile(r'\d+(\.\d+)?')
+    page_amo = int(re.findall(r'\d+',page_amo)[0])
+    amo      = int(re.findall(r'\d+(\.\d+)?',page_amo)[0]
+    i = 0
+    j=1
+    #循环取出记录,打开页面.
+    while i < rec_total:
+    #for j in range(0,page_amo):
+        #每页的记录 id为procurementAnnouncementShowList下级的li中的a标签
+        #li = page.locator('//*[@id="procurementAnnouncementShowList"]/li/a')
+        li = page.locator('#procurementAnnouncementShowList').locator("a")
+        #print(li.text_content())
+        #j+=1
+        for row in li.all():
+            data['序号'].append(i)
+            cc = base_url+str(row.get_attribute("href"))
+            data['href'].append(base_url+str(row.get_attribute("href")))
+            data['项目'].append(row.text_content())
+            print("序号:【%d】 %s , 【url】%s\n"%(i,cc,row.text_content()))
+            i += 1
+            di:dict = popup(context,row)
+            data['项目1'].append(di['prj_name'])
+            data['局办'].append(di['buyer'])   
+            data['类目'].append(di['buye_type'])
+            data['采购编号'].append(di['buyer_plan'])
+            data['金额'].append(di['budget'])
+            data['时间'].append(di['buyer_time'])
+            #print(data)
+        #每页做完,点下一页继续.
+        #ID为pagination下面的<li>></li> text是精准,不用has_text
+        print("记录i: %d , 第J页 is %d"%(i,j))
+        #if j <= page_amo-1: #最后一页不用点
+        if i < rec_total: 
+            page.locator('#pagination').locator('li:text(">")').click()
+            page.wait_for_timeout(2000)
+            #page.wait_for_load_state()
+            j += 1
+        
+
+    write_excel(data)
+    print("-----------end-------------")
+
+    
+    '''
+    查询totalpage，出来的是一个数组，
+    <li class="totalPage">…</li> aka get_by_text("跳转到页")
+    2) <li class="totalPage">共 104 页</li> aka get_by_text("共 104 页")
+    3) <li class="totalPage">每页 10 条</li> aka get_by_text("每页 10 条")
+    4) <li class="totalPage">合计 1031 条数据</li> aka get_by_text("合计 1031 条数据")
+    '''
+    #page.locator('//*[@class="totalPage"][2]').screenshot(path=f"totalpages.png")
+    #page.locator("text=/共 \s* 页").screenshot(path=f"totalpagesre.png")
+    #page.get_by_text(re.compile('^(共)*? 页$')).screenshot(path=f"totalpagesre.png") 正则错
+    #==================================================================
+    #page.locator("text=''/共%d页").screenshot(path=f"多少页.png")
+    page.locator('li:has-text("共")').screenshot(path=f"totalpagesre.png") 
+    print("------------------------")
+    print('多少页：%s ,\n inner text is : ',page.locator('li:has-text("共")').text_content())
+
+    for row in  page.locator('//*[@class="totalPage"]').all(): #page.get_by_role("listitem"). filter(has_text="共").all():
+        print(row.text_content())
+
+    print("------------------------")
+    page.close()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+def imgtostr():
+    url =f'https://gdgpo.czt.gd.gov.cn/freecms/verify/verifyCode.do?createTypeFlag=n&name=notice&d1678761066566'
+    response = requests.get(url)
+    response = response.content
+    ocr =  ddddocr.DdddOcr(old=True)
+    res =  ocr.classification(response)
+    print(res)
+
+    #写到内存中显示
+    BytesIOObj = BytesIO()
+    BytesIOObj.write(response)
+    img = Image.open(BytesIOObj)
+    img.show()
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        run(playwright)
+
+    #imgtostr()
